a1911ba6e42867d5bb22545be3fba43d59d34dd7
ricardosolares <65049081+ricardosolares@users.noreply.github.com>
2024-09-20 21:18:36 -0600

    Publicacion Principal

---
+++ reactfront/src/blog/CreateBlog.js
@@ -0,0 +1,49 @@
+import axios from 'axios'
+import { useState } from 'react'
+import { useNavigate } from 'react-router-dom'
+
+const URI = 'http://localhost:8000/blogs/'
+
+const CompCreateBlog = () => {
+    const [title, setTitle] = useState('')
+    const [content, setContent] = useState('')
+    const navigate = useNavigate()    
+    
+    //procedimiento guardar
+    const store = async (e) => {
+        e.preventDefault()
+        await axios.post(URI, {title: title, content:content})
+        navigate('/')
+    }   
+
+    return (
+        <div>
+           <h3>Create POST</h3>
+           <form onSubmit={store} style={{ maxWidth: '500px', margin: '0 auto' }}>
+    <div className='mb-3'>
+        <label className='form-label'>Titulo</label>
+        <input
+            value={title}
+            onChange={(e) => setTitle(e.target.value)}
+            type="text"
+            className='form-control'
+        />
+    </div>
+    <div className='mb-3'>
+        <label className='form-label'>Contenido</label>
+        <textarea
+            value={content}
+            onChange={(e) => setContent(e.target.value)}
+            type="text"
+            className='form-control'
+            style={{ width: '100%', height: '100px' }} // Ajusta el tamaño del textarea
+        />
+    </div>
+    <button type='submit' className='btn btn-primary'>Guardar</button>
+</form>
+
+        </div>
+    )
+}
+
+export default CompCreateBlog
---
+++ reactfront/src/blog/EditBlog.js
@@ -0,0 +1,64 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
+import { useNavigate, useParams } from "react-router-dom";
+
+const URI = 'http://localhost:8000/blogs/'
+
+const CompEditBlog = () => {
+    const [title, setTitle] = useState('')    
+    const [content, setContent] = useState('')    
+    const navigate = useNavigate()
+    const {id} = useParams()
+
+    //procedimiento para actualizar
+    const update = async (e) => {
+        e.preventDefault()
+        await axios.put(URI+id, {
+            title: title,
+            content: content
+        })
+        navigate('/')
+    }
+
+    useEffect( ()=>{
+        getBlogById()
+    },[])
+
+    const getBlogById = async () => {
+        const res = await axios.get(URI+id)
+        setTitle(res.data.title)
+        setContent(res.data.content)
+    }
+
+    return (
+        <div>
+        <h3>Edit POST</h3>
+        <form onSubmit={update} style={{ maxWidth: '500px', margin: '0 auto' }}>
+    <div className="mb-3">
+        <label className="form-label">Titulo</label>
+        <input
+            value={title}
+            onChange={(e) => setTitle(e.target.value)}
+            type="text"
+            className="form-control"
+        />
+    </div>
+    <div className="mb-3">
+        <label className="form-label">Contenido</label>
+        <textarea
+            value={content}
+            onChange={(e) => setContent(e.target.value)}
+            type="text"
+            className="form-control"
+            style={{ width: '100%', height: '100px' }} // Ajusta el tamaño del textarea
+        />
+    </div>
+    <button type="submit" className="btn btn-primary">Actualizar</button>
+</form>
+
+    </div>
+    )
+
+}
+
+export default CompEditBlog
